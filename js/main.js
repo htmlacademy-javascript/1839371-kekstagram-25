c5fa3c31b10f2ae0abe3b2c9ef22f9737cef2b13
Kirill Malskiy <malskykirill@yandex.ru>
2022-02-19 23:53:50 +0300

    Добавляет функции: Случайное число и Проверка длинны строки

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function randomInt(min, max) {
+
+  return (min < max && min >= 0) ? Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) + min : 'Диапазон задан некорректно';
+
+}
+
+
+function commentLength(str, maxLegth = 140) {
+
+  return str.length <= maxLegth;
+
+}
+
+
+randomInt(1, 4)
+commentLength('неnkm')
+
