98c79685bebcabe080bf724efa79dd9261ece634
Keks <keks@htmlacademy.ru>
2022-03-15 00:40:50 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/pictures.js
+++ 6/js/pictures.js
@@ -5,12 +5,13 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const similarPhotos = createPhotos();
 const similarPicturesFragment = document.createDocumentFragment();
 
-similarPhotos.forEach(({url, comments, likes}) => {
+similarPhotos.forEach(({url, comments, likes, id}) => {
   const pictureElement = pictureTemplate.cloneNode(true);
 
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__img').id = id;                       // добавил id для выбора миниатюры
 
   similarPicturesFragment.appendChild(pictureElement);
 });
@@ -26,29 +27,28 @@ const bigPictureClose = document.querySelector('.big-picture__cancel');
 const bigPictureImj = document.querySelector('#big-picture__img'); // добавил id на img полноэкранного режима
 const bigPictureSocial = document.querySelector('.big-picture__social');
 const likesCount = bigPictureSocial.querySelector('.likes-count');
+const socialCaption = bigPictureSocial.querySelector('.social__caption');
 const socialCommentCount = document.querySelector('.social__comment-count');
 const commentsCount = socialCommentCount.querySelector('.comments-count');
 const socialComments = document.querySelector('.social__comments');
-const socialComment = document.querySelector('.social__comment')
+const commentsLoader = document.querySelector('.comments-loader');
+const body = document.querySelector('body');
 
-console.log(bigPictureImj);
 
 // навесил обработчик открытия попапа по клику на миниатюру
 const addThumbnailClickHandler = function (thumbnail) {
-
   thumbnail.addEventListener('click', () => {
-
 
     bigPicture.classList.remove('hidden');
     bigPictureImj.src = thumbnail.querySelector('img').getAttribute('src');
     likesCount.textContent = thumbnail.querySelector('.picture__likes').textContent;
     commentsCount.textContent = thumbnail.querySelector('.picture__comments').textContent;
 
-    socialComments.innerHTML = '';
+    const id = thumbnail.querySelector('img').getAttribute('id');
 
-    for (let photo of similarPhotos) {
-      photo.comments.forEach(({avatar, name, message}) => {
-        bigPicture.querySelector('.social__comments').insertAdjacentHTML('beforeend', `
+    socialComments.innerHTML = '';
+    similarPhotos[id].comments.forEach(({avatar, name, message}) => {
+      bigPicture.querySelector('.social__comments').insertAdjacentHTML('beforeend', `
             <li class="social__comment">
               <img
                 class="social__picture"
@@ -57,19 +57,33 @@ const addThumbnailClickHandler = function (thumbnail) {
                 width="35" height="35">
               <p class="social__text">${message}</p>
             </li>`);
+    });
 
-      });
-    }
+    socialCaption.textContent = similarPhotos[id].description;
+
+    socialCommentCount.classList.add('hidden');
+    commentsLoader.classList.add('hidden');
+    body.classList.add('modal-open');
   });
-
 };
 
 // создал замыкание для выбора миниатюр
-
 for (let thumbnail of thumbnails) {
   addThumbnailClickHandler(thumbnail);
 }
 
-bigPictureClose.addEventListener('click', () => bigPicture.classList.add('hidden')); // закрытие попапа по нажатию на крестик
+// закрытие попапа по нажатию на крестик
+bigPictureClose.addEventListener('click', () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+});
+
+// закрытие попапа по нажатию на Esc
+document.addEventListener('keydown', (evt) => {
+  if(evt.key === 'Escape') {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+  }
+});
 
 
