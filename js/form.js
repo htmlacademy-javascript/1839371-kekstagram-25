0d74ba8806a3b77a22a19084c45ab58053fe7958
Kirill Malskiy <malskykirill@yandex.ru>
2022-03-24 20:10:44 +0300

    Помощь друга

--- js/form.js
+++ js/form.js
@@ -8,11 +8,162 @@ const textHashtagsElement = document.querySelector('.text__hashtags');
 const bodyElement = document.querySelector('body');
 const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,100}$/;
 
+const scaleControlSmallerElement = document.querySelector('.scale__control--smaller');
+const scaleControlBiggerEleent = document.querySelector('.scale__control--bigger');
+const scaleControlValueElement = document.querySelector('.scale__control--value');
+const imgUploadPreviewElement = document.querySelector('#img-upload__preview');
+
+const effectsRadioElements = document.querySelectorAll('.effects__radio');
+const imgUploadEffectLevelElement = document.querySelector('.img-upload__effect-level');
+const effectLevelValue = imgUploadEffectLevelElement.querySelector('.effect-level__value');
+
 // открытие модального окна
 uploadFileElement.addEventListener('click', () => {
   imgUploadOverlayElement.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
 });
+
+// изменение масштаба фотки
+function countScale () {
+  let fieldValue = parseFloat(scaleControlValueElement.value);
+  imgUploadPreviewElement.classList.add('scale');
+
+  scaleControlSmallerElement.addEventListener('click', () => {
+    if (fieldValue > 25) {
+      fieldValue -= 25;
+      imgUploadPreviewElement.style.transform = `scale(${fieldValue / 100})`;
+      scaleControlValueElement.value = `${fieldValue}%`;
+    }
+  });
+
+  scaleControlBiggerEleent.addEventListener('click', () => {
+    if (fieldValue < 100) {
+      fieldValue += 25;
+      imgUploadPreviewElement.style.transform = `scale(${fieldValue / 100})`;
+      scaleControlValueElement.value = `${fieldValue}%`;
+    }
+  });
+}
+
+countScale();
+
+// создание потягушки
+noUiSlider.create(imgUploadEffectLevelElement, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  start: 0.8,
+  step: 0.1,
+  connect: 'lower',
+});
+
+// изменение эффекта фотки
+imgUploadEffectLevelElement.classList.add('hidden');
+
+function selectEffect () {
+  for (const element of effectsRadioElements) {
+    element.addEventListener('change', () => {
+      for (const elem of effectsRadioElements) {
+        imgUploadPreviewElement.classList.remove(`effects__preview--${elem.value}`);
+      }
+      imgUploadPreviewElement.classList.add(`effects__preview--${element.value}`);
+
+      if (element.value === 'none') {
+        imgUploadEffectLevelElement.classList.add('hidden');
+        imgUploadPreviewElement.style.filter = null;
+      }
+
+      if (element.value === 'chrome') {
+        imgUploadEffectLevelElement.classList.remove('hidden');
+        imgUploadEffectLevelElement.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 1,
+          },
+          start: 0.8,
+          step: 0.1,
+          connect: 'lower',
+        });
+        imgUploadEffectLevelElement.noUiSlider.on('update', () => {
+          effectLevelValue.value = imgUploadEffectLevelElement.noUiSlider.get();
+          imgUploadPreviewElement.style.filter = `grayscale(${effectLevelValue.value})`;
+        });
+      }
+
+      if (element.value === 'sepia') {
+        imgUploadEffectLevelElement.classList.remove('hidden');
+        imgUploadEffectLevelElement.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 1,
+          },
+          start: 0.8,
+          step: 0.1,
+          connect: 'lower',
+        });
+        imgUploadEffectLevelElement.noUiSlider.on('update', () => {
+          effectLevelValue.value = imgUploadEffectLevelElement.noUiSlider.get();
+          imgUploadPreviewElement.style.filter = `sepia(${effectLevelValue.value})`;
+        });
+      }
+
+      if (element.value === 'marvin') {
+        imgUploadEffectLevelElement.classList.remove('hidden');
+        imgUploadEffectLevelElement.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 100,
+          },
+          start: 90,
+          step: 1,
+          connect: 'lower',
+        });
+        imgUploadEffectLevelElement.noUiSlider.on('update', () => {
+          effectLevelValue.value = imgUploadEffectLevelElement.noUiSlider.get();
+          imgUploadPreviewElement.style.filter = `invert(${effectLevelValue.value}%)`;
+        });
+      }
+
+      if (element.value === 'phobos') {
+        imgUploadEffectLevelElement.classList.remove('hidden');
+        imgUploadEffectLevelElement.noUiSlider.updateOptions({
+          range: {
+            min: 0,
+            max: 3,
+          },
+          start: 2,
+          step: 0.1,
+          connect: 'lower',
+        });
+        imgUploadEffectLevelElement.noUiSlider.on('update', () => {
+          effectLevelValue.value = imgUploadEffectLevelElement.noUiSlider.get();
+          imgUploadPreviewElement.style.filter = `blur(${effectLevelValue.value}px)`;
+        });
+      }
+
+      if (element.value === 'heat') {
+        imgUploadEffectLevelElement.classList.remove('hidden');
+        imgUploadEffectLevelElement.noUiSlider.updateOptions({
+          range: {
+            min: 1,
+            max: 3,
+          },
+          start: 2,
+          step: 0.1,
+          connect: 'lower',
+        });
+        imgUploadEffectLevelElement.noUiSlider.on('update', () => {
+          effectLevelValue.value = imgUploadEffectLevelElement.noUiSlider.get();
+          imgUploadPreviewElement.style.filter = `brightness(${effectLevelValue.value})`;
+        });
+      }
+    });
+  }
+}
+
+selectEffect();
+
 
 // скрытие модального окна по крестику
 imgUploadCancelElement.addEventListener('click', () => {
@@ -34,6 +185,7 @@ document.addEventListener('keydown', (evt) => {
   }
 });
 
+
 //валидация хештега
 const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
@@ -43,7 +195,6 @@ const pristine = new Pristine(form, {
   errorTextTag: 'p',
   errorTextClass: 'img-upload-error'
 });
-
 
 pristine.addValidator(textHashtagsElement, () => {
   const arrayHashtags = textHashtagsElement.value.split(' ');
